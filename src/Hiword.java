26e3278d03e3e2d234a05b35741dedacd1037093
wdj666 <1689151386@qq.com>
2020-05-11 13:55:01 +0800

    test

---
+++ src/Hiword.java
@@ -0,0 +1,21 @@
+/**
+ * @author wdj
+ * @date 2020/5/11 0011 13:40
+ */
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+/**
+ *@ClassName Hiword
+ *@Description TODO
+ *@Author Administrator
+ *@Date 2020/5/11 0011 13:40
+ *@Version 8.0
+ **/
+public class Hiword {
+    public static void main(String[] args) {
+        System.out.println("Hi World");
+    }
+}
